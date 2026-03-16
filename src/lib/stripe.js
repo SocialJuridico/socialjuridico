ed72ce0dc2643523a4463a82b4cc9132bd14fc67
MxSGameJPS <mxsgamejps@gmail.com>
2026-03-16 14:14:44 -0300

    feat: Initialize Stripe API client.

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,5 +1,7 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16', // Or latest
+const stripeKey = process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder';
+
+export const stripe = new Stripe(stripeKey, {
+  apiVersion: '2023-10-16',
 });

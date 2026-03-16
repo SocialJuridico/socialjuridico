1d5757d2e07f85bc80cfd2e483e9b4c120b68450
MxSGameJPS <mxsgamejps@gmail.com>
2026-03-16 09:48:43 -0300

    feat: Configure Next.js image remote patterns to allow images from ui-avatars.com and Supabase.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
